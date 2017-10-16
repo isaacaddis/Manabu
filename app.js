b97daa3468330fd8582249f9db654eee8f453be9
isaacaddis <isaacaddis21@gmail.com>
2017-10-15 23:36:07 -0700

    Setup Passport for User Registration

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var configDB = require('./config/database.js');
 
 var app = express();
+
+mongoose.connect(configDB.url);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +33,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+//passport stuff
+app.use(session({ secret: 'itslityah' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./app/routes.js')(app, passport);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
