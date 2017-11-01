93df80c170624d3b5298cd5e5a2862a2f446d5e5
isaacaddis <isaacaddis21@gmail.com>
2017-11-01 00:19:24 -0700

    No favicon for now lol

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'ejs');
 //Middlewares
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
